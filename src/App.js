7588ba46996bd9645c9a6f5d5fd6e9d24f6ff243
Vicente Reig Rincon de Arellano <vicente.reig@gmail.com>
2020-05-23 12:25:28 +0200

    Project setup
    - Cleaning the boilerplate code generated with create-react-app.
    - Installing and setting up MDX to support the interactive
      walkthrough.
    - Installs Tailwind CSS depedency.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense} from 'react'
+import { importMDX } from 'mdx.macro'
+import './App.css'
+
+const Content = lazy(() => importMDX('./Content/index.mdx'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overview-page">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content/>
+      </Suspense>
+
     </div>
-  );
+  )
 }
 
 export default App;
